cc43b454f7959f85971be721abb3c8b7b7fd5d30
Michael Thorne <michael.thorne@gmail.com>
2017-10-13 08:51:21 +0200

    WIP

--- vue-components/src/Home.vue
+++ vue-components/src/Home.vue
@@ -1,27 +1,14 @@
 <template>
-    <!-- Component template should only contain one root element -->
     <div>
-        <p>Server Status: {{ status }}</p>
-        <hr>
-        <button @click="changeStatus">Change Status</button>
+        <app-server-status v-for="server in 5"></app-server-status>
     </div>
 </template>
 
 <script>
+    import ServerStatus from './ServerStatus.vue'
     export default {
-        data: function () {
-            return {
-                status: 'Critical'
-            }
-        },
-        methods: {
-            changeStatus () {
-                this.status = 'Normal';
-            }
+        components: {
+            'app-server-status': ServerStatus
         }
     }
 </script>
-
-<style>
-
-</style>
---
+++ vue-components/src/ServerStatus.vue
@@ -0,0 +1,23 @@
+<template>
+    <!-- Component template should only contain one root element -->
+    <div>
+        <p>Server Status: {{ status }}</p>
+        <hr>
+        <button @click="changeStatus">Change Status</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function () {
+            return {
+                status: 'Critical'
+            }
+        },
+        methods: {
+            changeStatus () {
+                this.status = 'Normal';
+            }
+        }
+    }
+</script>
