8b4faf4c2ba382d5040e9c70814a3c6dff9f6704
Michael Thorne <michael.thorne@gmail.com>
2017-10-16 12:45:35 +0200

    WIP

--- communicating-between-components/src/components/User.vue
+++ communicating-between-components/src/components/User.vue
@@ -6,7 +6,7 @@
         <hr>
         <div class="row">
             <div class="col-xs-12 col-sm-6">
-                <app-user-detail :myName="myName"></app-user-detail>
+                <app-user-detail :myName="myName" @nameWasReset="myName = $event"></app-user-detail>
             </div>
             <div class="col-xs-12 col-sm-6">
                 <app-user-edit></app-user-edit>
--- communicating-between-components/src/components/UserDetail.vue
+++ communicating-between-components/src/components/UserDetail.vue
@@ -3,15 +3,36 @@
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
         <p>User Name: {{ switchName() }}</p>
+        <button @click="resetName">Reset Name</button>
     </div>
 </template>
 
 <script>
     export default {
-        props: ['myName'],
+        props: {
+            // Validation for multiple types
+            // myName: [String, Array]
+            myName: {
+                type: String,
+                required: true,
+                // default: 'Michael'
+            },
+            /*myAge: {
+                type: Object,
+                default: function () {
+                    return {
+                        age: '35'
+                    }
+                }
+            }*/
+        },
         methods: {
             switchName () {
                 return this.myName.split('').reverse().join('');
+            },
+            resetName () {
+                this.myName = 'Michael';
+                this.$emit('nameWasReset', this.myName);
             }
         }
     }
