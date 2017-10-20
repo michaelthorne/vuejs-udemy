ccec8be0d2dca696c52fe56d432e006ad398be3e
Michael Thorne <michael.thorne@gmail.com>
2017-10-20 11:16:24 +0200

    WIP

---
+++ vue-slots/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm">
+                <app-quote>
+                    <h2 slot="title">{{ quoteTitle }}</h2>
+                    <span slot="subtitle">Subtitle goes here.</span>
+                    <p>The contents of my quote</p>
+                </app-quote>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Quote from './components/Quote.vue';
+
+    export default {
+        components: {
+            appQuote: Quote
+        },
+        data () {
+            return {
+                'quoteTitle': 'This is the title of my quote'
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ vue-slots/src/components/Quote.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <div>
+            <slot name="title"></slot>
+            <span style="font-style: italic">
+                <slot name="subtitle">This is the default subtitle.</slot>
+            </span>
+        </div>
+        <hr>
+        <div>
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {}
+</script>
+
+<style scoped>
+    div {
+        border: 0.125em solid #ccc;
+        box-shadow: 0.125em 0.125em 0.25em #000;
+        margin: 2em auto;
+        padding: 2em;
+        text-align: center;
+    }
+
+    h2 {
+        color: red;
+    }
+
+    p {
+        color: blue;
+    }
+</style>
