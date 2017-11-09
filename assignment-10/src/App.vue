213ad7f289f26297e8076066073c0bd8c65824c7
Michael Thorne <michael.thorne@gmail.com>
2017-11-09 08:00:38 +0200

    WIP

--- assignment-10/src/App.vue
+++ assignment-10/src/App.vue
@@ -2,10 +2,10 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1 v-myon="'notify'">Directives Exercise</h1>
+                <h1>Directives Exercise</h1>
                 <!-- Exercise -->
                 <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
-
+                <button v-customOn:click="clicked" class="btn btn-primary">Button</button>
             </div>
         </div>
     </div>
@@ -14,12 +14,15 @@
 <script>
     export default {
         directives: {
-            'myon': {
-                bind (el, binding, vnode) {
-                    if (binding.value == 'notify') {
-                        alert('Hello, World!');
-                    }
+            customOn: {
+                bind (el, binding) {
+                    el.addEventListener(binding.arg, binding.value);
                 }
+            }
+        },
+        methods: {
+            clicked () {
+                alert('I was clicked!');
             }
         }
     }
