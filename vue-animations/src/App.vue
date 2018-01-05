00b68547f593ecc5518f267a049e04ba7b127f95
Michael Thorne <michael.thorne@gmail.com>
2018-01-05 16:19:11 +0200

    Vue Animations

---
+++ vue-animations/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Animations</h1>
+        <hr>
+        <button class="btn btn-primary" @click="show = !show">Show Alert</button>
+        <br><br>
+        <transition name="fade">
+          <div class="alert alert-info" v-if="show">This is some information</div>
+        </transition>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        show: false
+      };
+    },
+  };
+</script>
+
+<style>
+  .fade-enter,
+  .fade-leave {
+    opacity: 0;
+  }
+
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: opacity 0.5s;
+  }
+</style>
