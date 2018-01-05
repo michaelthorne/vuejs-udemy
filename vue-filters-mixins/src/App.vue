7b8fd4556289fa568ee86d495f7c43cb383e92ed
Michael Thorne <michael.thorne@gmail.com>
2018-01-05 12:29:36 +0200

    Vue Mixins

--- vue-filters-mixins/src/App.vue
+++ vue-filters-mixins/src/App.vue
@@ -3,40 +3,42 @@
     <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Filters & Mixins</h1>
+        <h2>Filters</h2>
         <p>{{ text | toUppercase }}</p>
         <p>{{ text | toLowercase }}</p>
         <hr>
-        <h2>Computed Properties</h2>
+        <h2>Filters with Computed Properties</h2>
         <input type="text" v-model="filterText">
+        <hr>
         <ul>
           <li v-for="fruit in filteredFruits">{{ fruit }}</li>
         </ul>
+        <hr>
+        <app-list></app-list>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+  import List from './List.vue';
+  import { fruitMixin } from './fruitMixin';
+
   export default {
     data () {
       return {
         text: 'Hello there!',
-        fruits: ['Apple', 'Banana', 'Orange', 'Melon'],
-        filterText: '',
       };
+    },
+    components: {
+      appList: List,
     },
     filters: {
       toUppercase (value) {
         return value.toUpperCase();
       },
     },
-    computed: {
-      filteredFruits () {
-        return this.fruits.filter((element) => {
-          return element.match(this.filterText);
-        });
-      },
-    },
+    mixins: [fruitMixin],
   };
 </script>
 
---
+++ vue-filters-mixins/src/List.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>Filters with Computed Properties</h2>
+    <input type="text" v-model="filterText">
+    <hr>
+    <ul>
+      <li v-for="fruit in filteredFruits">{{ fruit }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  import { fruitMixin } from './fruitMixin';
+
+  export default {
+    mixins: [fruitMixin],
+  };
+</script>
+
+<style>
+
+</style>
