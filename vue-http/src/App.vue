18c4e6950463ee61ba5e617b6cbecb5daea86b5f
Michael Thorne <michael.thorne@gmail.com>
2018-01-22 10:33:37 +0200

    WIP

--- vue-http/src/App.vue
+++ vue-http/src/App.vue
@@ -3,7 +3,7 @@
     <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>HTTP</h1>
-        <h2>Vue Resource</h2>
+        <h2>Vue Resource + Firebase</h2>
 
         <hr>
 
@@ -20,6 +20,11 @@
         <button class="btn btn-primary" @click="submit">Submit</button>
 
         <hr>
+
+        <div class="form-group">
+          <label for="node">Node</label>
+          <input class="form-control" id="node" type="text" v-model="node">
+        </div>
 
         <button class="btn btn-primary" @click="fetchData">Get Data</button>
 
@@ -41,28 +46,44 @@
           username: '',
           email: ''
         },
-        users: []
+        users: [],
+        resource: {},
+        node: 'data'
       }
     },
     methods: {
       submit () {
         // console.log(this.user)
-        this.$http.post('', this.user) // URL is stored in `main.js`
-          .then(response => {
-            console.log(response)
-          }, error => {
-            console.log(error)
-          })
+        // this.$http.post(data.json, this.user) // URL is stored in `main.js`
+        //   .then(response => {
+        //     console.log(response)
+        //   }, error => {
+        //     console.log(error)
+        //   })
+        this.resource.save({node: this.node}, this.user)
+        // this.resource.saveAlt(this.user)
       },
       fetchData () {
-        this.$http.get('') // URL is stored in `main.js`
+        // this.$http.get('') // URL is stored in `main.js`
+        //   .then(response => {
+        //     // const data = response.json() // Returns a Promise, not the data.
+        //     // console.log(data)
+        //     return response.json()
+        //   })
+        //   .then(data => {
+        //     // console.log(data)
+        //     const resultArray = []
+        //     for (let key in data) {
+        //       resultArray.push(data[key])
+        //     }
+        //     this.users = resultArray
+        //   })
+
+        this.resource.getData({node: this.node})
           .then(response => {
-            // const data = response.json() // Returns a Promise, not the data.
-            // console.log(data)
             return response.json()
           })
           .then(data => {
-            // console.log(data)
             const resultArray = []
             for (let key in data) {
               resultArray.push(data[key])
@@ -70,6 +91,13 @@
             this.users = resultArray
           })
       }
+    },
+    created () {
+      const customActions = {
+        saveAlt: {method: 'POST', url: 'alternative.json'},
+        getData: {method: 'GET'}
+      }
+      this.resource = this.$resource('{node}.json', {}, customActions)
     }
   }
 </script>
--- vue-http/src/main.js
+++ vue-http/src/main.js
@@ -4,24 +4,20 @@ import App from './App.vue'
 
 Vue.use(VueResource)
 
-Vue.http.options.root = 'https://vuejs-http-78bb4.firebaseio.com/data.json'
+Vue.http.options.root = 'https://vuejs-http-78bb4.firebaseio.com/'
 // Vue.http.options.headers = ''
 Vue.http.interceptors.push((request, next) => {
-  console.log(request)
-
   if (request.method === 'POST') {
     // PUT overwrites the old data on Firebase.
     // request.method = 'PUT'
   }
   next(response => {
-    /*
-    response.json = () => {
-      return {
-        // Extract response body and store it in a key with an object
-        messages: response.body
-      }
-    }
-    */
+    // response.json = () => {
+    //   return {
+    //     // Extract response body and store it in a key with an object
+    //     messages: response.body
+    //   }
+    // }
   })
 })
 
