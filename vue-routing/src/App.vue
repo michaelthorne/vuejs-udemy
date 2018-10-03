f135891dff699d9ff10528368f9f11b4a13936af
Michael Thorne <michael.thorne@gmail.com>
2018-10-03 14:48:51 +0200

    WIP

--- vue-routing/src/App.vue
+++ vue-routing/src/App.vue
@@ -5,7 +5,9 @@
         <h1>Routing</h1>
         <hr>
         <router-view name="header-top"></router-view>
-        <router-view></router-view>
+        <transition name="slide" mode="out-in">
+          <router-view></router-view>
+        </transition>
         <router-view name="header-bottom"></router-view>
       </div>
     </div>
@@ -22,4 +24,48 @@
 </script>
 
 <style>
+  .slide-enter {
+    opacity: 0;
+  }
+
+  .slide-enter-active {
+    animation: slide-in 0.5s ease-out forwards;
+    transition: opacity 0.5s;
+  }
+
+  .slide-leave {
+
+  }
+
+  .slide-leave-active {
+    animation: slide-out 0.5s ease-out forwards;
+    opacity: 0;
+    position: absolute;
+    transition: opacity 0.5s;
+  }
+
+  /* Attached to any element that needs to change it's place (after adding or deleting an element). */
+  .slide-move {
+    transition: transform 1s;
+  }
+
+  @keyframes slide-in {
+    from {
+      transform: translateY(1em);
+    }
+
+    to {
+      transform: translateY(0);
+    }
+  }
+
+  @keyframes slide-out {
+    from {
+      transform: translateY(0);
+    }
+
+    to {
+      transform: translateY(1em);
+    }
+  }
 </style>
