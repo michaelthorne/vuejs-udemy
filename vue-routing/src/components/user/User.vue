ef676a523540d3b80fdacdc614ea52663ad36ccc
Michael Thorne <michael.thorne@gmail.com>
2018-10-03 14:17:28 +0200

    Routing WIP

--- vue-routing/src/components/user/User.vue
+++ vue-routing/src/components/user/User.vue
@@ -2,27 +2,17 @@
   <div>
     <h1>The User Page</h1>
     <hr>
-    <p>Loaded User ID: {{ id }}</p>
-    <br>
     <button class="btn btn-secondary" @click="navigateToHome">Go to Home</button>
+    <hr>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
   export default {
-    data () {
-      return {
-        id: this.$route.params.id
-      }
-    },
     methods: {
       navigateToHome () {
-        this.$router.push({path: '/'})
-      }
-    },
-    watch: {
-      '$route' (to, from) {
-        this.id = to.params.id
+        this.$router.push({name: 'home'})
       }
     }
   }
--- vue-routing/src/routes/index.js
+++ vue-routing/src/routes/index.js
@@ -1,13 +1,31 @@
 import Home from '../components/Home.vue'
 import User from '../components/user/User.vue'
+import UserDetail from '../components/user/UserDetail'
+import UserEdit from '../components/user/UserEdit.vue'
+import UserStart from '../components/user/UserStart.vue'
 
 export const routes = [
   {
     path: '',
-    component: Home
+    component: Home,
+    name: 'home'
   },
   {
-    path: '/user/:id',
-    component: User
+    path: '/user',
+    component: User, children: [
+      {
+        path: '',
+        component: UserStart
+      },
+      {
+        path: ':id',
+        component: UserDetail
+      },
+      {
+        path: ':id/edit',
+        component: UserEdit,
+        name: 'userEdit'
+      }
+    ]
   }
 ]
